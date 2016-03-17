297232abfad32b97c0bbe7fd4e52ee5add961931
salhidev <salhi.dev@gmail.com>
2016-03-17 08:08:51 -0700

    Added files via upload

---
+++ app.js
@@ -0,0 +1,150 @@
+// Application object.
+var app = {}
+
+// Regions that define which page to show for each beacon.
+app.beaconRegions =
+[
+	{
+		id: 'Pg-Admissions',
+		uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
+		major: 64090,
+		minor: 39177
+	},
+	{
+		id: 'Pg-Welcome',
+		uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
+		major: 39645,
+		minor: 43357
+	},
+	{
+		id: 'Pg-Bursar',
+		uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
+		major: 60318,
+		minor: 39220
+	}
+]
+
+// Currently displayed page.
+app.currentPage = 'Pg-Welcome'
+
+app.initialize = function()
+{
+	document.addEventListener(
+		'deviceready',
+		app.onDeviceReady,
+		false)
+	app.gotoPage(app.currentPage)
+}
+
+// Called when Cordova are plugins initialised,
+// the iBeacon API is now available.
+app.onDeviceReady = function()
+{
+	// Specify a shortcut for the location manager that
+	// has the iBeacon functions.
+	window.locationManager = cordova.plugins.locationManager
+
+	// Start tracking beacons!
+	app.startScanForBeacons()
+}
+
+app.startScanForBeacons = function()
+{
+	//console.log('startScanForBeacons')
+
+	// The delegate object contains iBeacon callback functions.
+	var delegate = new cordova.plugins.locationManager.Delegate()
+
+	delegate.didDetermineStateForRegion = function(pluginResult)
+	{
+		//console.log('didDetermineStateForRegion: ' + JSON.stringify(pluginResult))
+	}
+
+	delegate.didStartMonitoringForRegion = function(pluginResult)
+	{
+		//console.log('didStartMonitoringForRegion:' + JSON.stringify(pluginResult))
+	}
+
+	delegate.didRangeBeaconsInRegion = function(pluginResult)
+	{
+		//console.log('didRangeBeaconsInRegion: ' + JSON.stringify(pluginResult))
+		app.didRangeBeaconsInRegion(pluginResult)
+	}
+
+	// Set the delegate object to use.
+	locationManager.setDelegate(delegate)
+
+	// Start monitoring and ranging our beacons.
+	for (var r in app.beaconRegions)
+	{
+		var region = app.beaconRegions[r]
+
+		var beaconRegion = new locationManager.BeaconRegion(
+			region.id, region.uuid, region.major, region.minor)
+
+		// Start monitoring.
+		locationManager.startMonitoringForRegion(beaconRegion)
+			.fail(console.error)
+			.done()
+
+		// Start ranging.
+		locationManager.startRangingBeaconsInRegion(beaconRegion)
+			.fail(console.error)
+			.done()
+	}
+}
+
+// Display pages depending of which beacon is close.
+app.didRangeBeaconsInRegion = function(pluginResult)
+{
+	//console.log('numbeacons in region: ' + pluginResult.beacons.length)
+
+	// There must be a beacon within range.
+	if (0 == pluginResult.beacons.length)
+	{
+		return
+	}
+
+	// Our regions are defined so that there is one beacon per region.
+	// Get the first (and only) beacon in range in the region.
+	var beacon = pluginResult.beacons[0]
+
+	// The region identifier is the page id.
+	var pageId = pluginResult.region.identifier
+
+	//console.log('ranged beacon: ' + pageId + ' ' + beacon.proximity)
+
+	// If the beacon is close and represents a new page, then show the page.
+	if ((beacon.proximity == 'ProximityImmediate' || beacon.proximity == 'ProximityNear'||beacon.proximity == 'ProximityFar')
+		&& app.currentPage == 'Pg-default' || app.currentPage == 'Pg-Admissions' || app.currentPage == 'Pg-Welcome' || app.currentPage == 'Pg-Bursar')
+	{
+		app.gotoPage(pageId)
+		return
+	}
+	
+}
+
+app.gotoPage = function(pageId)
+{
+	app.hidePage(app.currentPage)
+	app.showPage(pageId)
+	app.currentPage = pageId
+}
+
+app.showPage = function(pageId)
+{
+	document.getElementById(pageId).style.display = 'block'
+}
+
+app.hidePage = function(pageId)
+{
+	document.getElementById(pageId).style.display = 'none'
+}
+
+// Set up the application.
+app.initialize()
+jQuery(document).ready(function($) {
+    $(".page-default").ready(function() {
+        ga('send','event', 'Herbstaktion', 'Gravity Form abgesendet');
+    });
+});
---
+++ divtracker.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($) {
+    $(".page-default").ready(function() {
+        ga_storage._trackEvent('Home-Page', 'Home-Page', 'Home-Page', 0);
+    });
+    });
+jQuery(document).ready(function($) {
+    $(".page-default1").ready(function() {
+        ga_storage._trackEvent('Home-Refresh', 'Home-Refresh', 'Home-Refresh', 0);
+    });
+    });
+jQuery(document).ready(function($) {
+    $(".page-feet").ready(function() {
+        ga_storage._trackEvent('Page-Survey', 'Page-Survey', 'Page-Survey', 0);
+    });
+    });
+jQuery(document).ready(function($) {
+    $(".page-face").ready(function() {
+        ga_storage._trackEvent('Page-BlueBeacon', 'Page-BlueBeacon', 'Page-BlueBeacon', 0);
+    });
+    });
+jQuery(document).ready(function($) {
+    $(".page-shoulders").ready(function() {
+        ga_storage._trackEvent('Page-Presentation', 'Page-Presentation', 'Page-Presentation', 0);
+    });
+    });
